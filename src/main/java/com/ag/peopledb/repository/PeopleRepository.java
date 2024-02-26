8d11181afeec98bb4e77cd9e23a3c1c5f0012f3a
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-02-26 19:58:27 +0100

    Lesson 235: Fetching children in one join - part 2

--- src/main/java/com/ag/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/ag/peopledb/repository/PeopleRepository.java
@@ -11,7 +11,6 @@ import java.sql.*;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Optional;
-import java.util.Set;
 
 import static java.util.stream.Collectors.joining;
 
@@ -24,14 +23,16 @@ public class PeopleRepository extends CRUDRepository<Person> {
             VALUES(?, ?, ?, ?, ?, ?, ?, ?) 
             """;
     public static final String FIND_BY_ID_SQL = """
-            SELECT 
-            P.ID, P.FIRST_NAME, P.LAST_NAME, P.DOB, P.SALARY, P.HOME_ADDRESS,
-            HOME.ID AS HOME_ID, HOME.STREET_ADDRESS AS HOME_STREET_ADDRESS, HOME.ADDRESS2 AS HOME_ADDRESS2, HOME.CITY AS HOME_CITY, HOME.STATE AS HOME_STATE, HOME.POSTCODE AS HOME_POSTCODE, HOME.COUNTY AS HOME_COUNTY, HOME.REGION AS HOME_REGION, HOME.COUNTRY AS HOME_COUNTRY, 
+            SELECT
+            PARENT.ID AS PARENT_ID, PARENT.FIRST_NAME AS PARENT_FIRST_NAME, PARENT.LAST_NAME AS PARENT_LAST_NAME, PARENT.DOB AS PARENT_DOB, PARENT.SALARY AS PARENT_SALARY, PARENT.EMAIL AS PARENT_EMAIL, 
+            CHILD.ID AS CHILD_ID, CHILD.FIRST_NAME AS CHILD_FIRST_NAME, CHILD.LAST_NAME AS CHILD_LAST_NAME, CHILD.DOB AS CHILD_DOB, CHILD.SALARY AS CHILD_SALARY, CHILD.EMAIL AS CHILD_EMAIL, 
+            HOME.ID AS HOME_ID, HOME.STREET_ADDRESS AS HOME_STREET_ADDRESS, HOME.ADDRESS2 AS HOME_ADDRESS2, HOME.CITY AS HOME_CITY, HOME.STATE AS HOME_STATE, HOME.POSTCODE AS HOME_POSTCODE, HOME.COUNTY AS HOME_COUNTY, HOME.REGION AS HOME_REGION, HOME.COUNTRY AS HOME_COUNTRY,
             BUSINESS.ID AS BUSINESS_ID, BUSINESS.STREET_ADDRESS AS BUSINESS_STREET_ADDRESS, BUSINESS.ADDRESS2 AS BUSINESS_ADDRESS2, BUSINESS.CITY AS BUSINESS_CITY, BUSINESS.STATE AS BUSINESS_STATE, BUSINESS.POSTCODE AS BUSINESS_POSTCODE, BUSINESS.COUNTY AS BUSINESS_COUNTY, BUSINESS.REGION AS BUSINESS_REGION, BUSINESS.COUNTRY AS BUSINESS_COUNTRY, 
-            FROM PEOPLE AS P
-            LEFT OUTER JOIN ADDRESSES AS HOME ON P.HOME_ADDRESS = HOME.ID
-            LEFT OUTER JOIN ADDRESSES AS BUSINESS ON P.BUSINESS_ADDRESS = BUSINESS.ID
-            WHERE P.ID=?
+            FROM PEOPLE AS PARENT
+            LEFT OUTER JOIN PEOPLE AS CHILD ON PARENT.ID = CHILD.PARENT_ID
+            LEFT OUTER JOIN ADDRESSES AS HOME ON PARENT.HOME_ADDRESS = HOME.ID
+            LEFT OUTER JOIN ADDRESSES AS BUSINESS ON PARENT.BUSINESS_ADDRESS = BUSINESS.ID
+            WHERE PARENT.ID = ?
             """;
     public static final String FIND_ALL_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE";
     public static final String SELECT_COUNT_SQL = "SELECT COUNT(*) FROM PEOPLE";
@@ -95,19 +96,39 @@ public class PeopleRepository extends CRUDRepository<Person> {
     @SQL(value = DELETE_BY_ID_SQL, operationType = CrudOperation.DELETE_ONE)
     @SQL(value = DELETE_BY_ID_IN_SQL, operationType = CrudOperation.DELETE_MANY)
     Person extractEntityFromResultSet(ResultSet resultSet) throws SQLException{
-        long personId = resultSet.getLong("ID");
-        String personFirstName = resultSet.getString("FIRST_NAME");
-        String personLastName = resultSet.getString("LAST_NAME");
-        ZonedDateTime personDateOfBirth = ZonedDateTime.of(resultSet.getTimestamp("DOB").toLocalDateTime(), ZoneId.of("+0"));
-        BigDecimal personSalary = resultSet.getBigDecimal("SALARY");
-//        long homeAddressId = resultSet.getLong("HOME_ADDRESS");
+        Person finalParent = null;
+        do {
+            Person currentParent = extractPerson(resultSet, "PARENT_");
 
-        Address homeAddress = extractAddress(resultSet, "HOME_");
-        Address businessAddress = extractAddress(resultSet, "BUSINESS_");
+            if(finalParent == null) {
+                finalParent = currentParent;
+            }
 
+            if(finalParent.equals(currentParent)){
+
+            }
+
+            Person child = extractPerson(resultSet, "CHILD_");
+
+            Address homeAddress = extractAddress(resultSet, "HOME_");
+            Address businessAddress = extractAddress(resultSet, "BUSINESS_");
+
+            finalParent.setHomeAddress(homeAddress);
+            finalParent.setBusinessAddress(businessAddress);
+            finalParent.addChild(child);
+
+        } while (resultSet.next());
+
+        return finalParent;
+    }
+
+    private static Person extractPerson(ResultSet resultSet, String aliasPrefix) throws SQLException {
+        long personId = getValueByAlias(aliasPrefix + "ID", resultSet, Long.class);
+        String personFirstName = getValueByAlias(aliasPrefix + "FIRST_NAME", resultSet, String.class);
+        String personLastName = getValueByAlias(aliasPrefix + "LAST_NAME", resultSet, String.class);
+        ZonedDateTime personDateOfBirth = ZonedDateTime.of(getValueByAlias(aliasPrefix + "DOB", resultSet, Timestamp.class).toLocalDateTime(), ZoneId.of("+0"));
+        BigDecimal personSalary = getValueByAlias(aliasPrefix + "SALARY", resultSet, BigDecimal.class);
         Person person = new Person(personId, personFirstName, personLastName, personDateOfBirth, personSalary);
-        person.setHomeAddress(homeAddress);
-        person.setBusinessAddress(businessAddress);
         return person;
     }
 
@@ -130,7 +151,7 @@ public class PeopleRepository extends CRUDRepository<Person> {
         return address;
     }
 
-    private <T> T getValueByAlias(String alias, ResultSet resultSet, Class<T> clazz) throws SQLException {
+    private static <T> T getValueByAlias(String alias, ResultSet resultSet, Class<T> clazz) throws SQLException {
         int columnCount = resultSet.getMetaData().getColumnCount();
         for(int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
             if(alias.equals(resultSet.getMetaData().getColumnLabel(columnIndex))){
